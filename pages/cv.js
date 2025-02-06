d91bfb5f07f175cc9a6e0af0d58d604f843d4be1
new name <caatkety@gmail.com>
2025-02-06 13:36:01 +0800

    hi

--- pages/cv.js
+++ pages/cv.js
@@ -18,7 +18,7 @@ export default function CV() {
             <div className="h-10 w-full border-b-4 border-black bg-green-200	background-color: rgb(187 247 208);">
                 <div className="my-6 flex flex-direction: row; grid justify-items-stretch ">
                     <div className="flex flex-direction: row; flex space-x-2 w-20">
-                    <div className="h-5 w-5 rounded-2xl bg-orange-500 background-color:#f97316 border-2 border-black border-color: rgb(0 0 0);"></div>
+                    <div className="h-6 w-5 rounded-2xl bg-orange-500 background-color:#f97316 border-2 border-black border-color: rgb(0 0 0);"></div>
                     <div className="h-5 w-5 rounded-2xl bg-red-500 background-color:#ef4444 border-2 border-black border-color: rgb(0 0 0);"></div>
                     <div className="h-5 w-5 rounded-2xl bg-green-700 background-color:#15803d border-2 border-black border-color: rgb(0 0 0);"></div>
                     </div>
